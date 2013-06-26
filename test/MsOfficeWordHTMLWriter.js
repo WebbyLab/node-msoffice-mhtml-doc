dca3175753efa48dd7cc97260ce88090c21b2e9f
koma <maks@komaval.com>
2013-06-26 20:11:19 +0300

    qunit for tests. removed pl

--- test/MsOfficeWordHTMLWriter.js
+++ test/MsOfficeWordHTMLWriter.js
@@ -1,33 +1,25 @@
-var testCase = require('nodeunit').testCase,
-    MsOfficeWordHTMLWriter = require("../lib/MsOfficeWordHTMLWriter.js");
+var MsOfficeWordHTMLWriter = require( __dirname + '/../lib/MsOfficeWordHTMLWriter.js');
 
-exports["Base usage"] = {
-    "Create document": function(test) {
-        var doc = new MsOfficeWordHTMLWriter({
-            title: "Demo",
-            WordDocument: { 
-                View: 'Print',
-                Compatibility: {
-                    DoNotExpandShiftReturn: ""
-                }
-            }
-        });
-
-        doc.write("hello, world");
-        var br = doc.page_break('right');
-        doc.write( br + 'new page after manual break' );
-        doc.create_section( { new_page: 'right' } );
-        doc.write('new page after section break');
-
-        var content = doc.content();
-
-        test.ok( /<w:View>Print<\/w:View>/g.test(content), "View => print" );
-        test.ok( /<w:Compatibility><w:DoNotExpandShiftReturn \/>/g.test(content), "Compatibility => DoNotExpandShiftReturn" );
-
-        var break_right_count = content.match(/page-break-before:right/g).length;
-
-        test.equal( break_right_count, 2, "page break:right" );
-
-        test.done();
+var doc = new MsOfficeWordHTMLWriter({
+    title: "Demo",
+    WordDocument: { 
+        View: 'Print',
+        Compatibility: {
+            DoNotExpandShiftReturn: ""
+        }
     }
-};
+});
+
+doc.write("hello, world");
+doc.write( doc.page_break('right') + 'new page after manual break' );
+doc.create_section( { new_page: 'right' } );
+doc.write('new page after section break');
+
+var content = doc.content();
+var break_right_count = content.match(/page-break-before:right/g).length;
+
+test('Create document', function(assert) {
+    ok( /<w:View>Print<\/w:View>/g.test(content), "View => print" );
+    ok( /<w:Compatibility><w:DoNotExpandShiftReturn \/>/g.test(content), "Compatibility => DoNotExpandShiftReturn" );
+    equal( break_right_count, 2, "page break:right" );
+});
---
+++ test/testrunner.js
@@ -0,0 +1,8 @@
+var testrunner = require("qunit");
+
+testrunner.run([
+    {
+        code: __dirname + "/MsOfficeWordHTMLWriter.js",
+        tests: __dirname + "/MsOfficeWordHTMLWriter.js"
+    }
+]);
