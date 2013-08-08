2bf62110ced95a67cd3aacbef2239c130066b0c2
koma <maks@komaval.com>
2013-08-08 16:01:29 +0300

    hot fix buffer handling

--- test/MsOfficeWordHTMLWriter.js
+++ test/MsOfficeWordHTMLWriter.js
@@ -1,4 +1,4 @@
-var MsOfficeWordHTMLWriter = require( __dirname + '/../lib/MsOfficeWordHTMLWriter' );
+var MsOfficeWordHTMLWriter = require( __dirname + '/../index.js' );
 
 var doc = new MsOfficeWordHTMLWriter({
     title: "Demo",
